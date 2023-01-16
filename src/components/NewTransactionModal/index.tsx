19b3ac6b835bfff469825264917265db8ac3ae81
Arthur Kavagouth Vasque <arthurkvasque.eng@outlook.com>
2023-01-16 17:17:58 -0300

    finish lesson 01

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,7 @@
 import * as Dialog from '@radix-ui/react-dialog';
-import { X } from 'phosphor-react';
-import { Content, Overlay } from './styles';
+import * as RadioGroup from '@radix-ui/react-radio-group';
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
+import { Content, Overlay, TransactionType, TransactionTypeButton } from './styles';
 
 export function NewTransactionModal() {
   return (
@@ -15,6 +16,21 @@ export function NewTransactionModal() {
         <input type="text" placeholder='Descrição' required/>
         <input type="number" placeholder='Preço' required/>
         <input type="text" placeholder='Categoria' required/>
+
+        
+        <TransactionType>
+          <TransactionTypeButton variant='income' value='income'>
+            <ArrowCircleUp size={24} />
+            Entrada
+          </TransactionTypeButton>
+        </TransactionType>
+
+        <TransactionType>
+          <TransactionTypeButton variant='outcome' value='outcome'>
+            <ArrowCircleDown size={24} />
+            Saída
+          </TransactionTypeButton>
+        </TransactionType>
 
         <button type='submit'>Cadastrar</button>
       </form>    
